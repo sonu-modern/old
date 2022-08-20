7226112d1ec4916218b7b5efea7959e8bfe76322
Nitin-440 <56361554+Nitin-440@users.noreply.github.com>
2022-08-20 17:00:58 +0530

    Update Downloader.py

--- cheggscraper/Downloader.py
+++ cheggscraper/Downloader.py
@@ -1,52 +1,40 @@
-from cheggscraper import Downloader
-import discord
-import discord.ext
-from discord.ext import commands
-#import inviter
-import asyncio
-import os
+import argparse
 import json
+import os
+from importlib.resources import read_text
 
-bot = discord.Client()
-bot = commands.Bot(command_prefix='#')
-#tracker = inviter.InviteTracker(bot)
-
-
-
-@bot.event
-async def on_member_join(member):
-    pass
+from .CheggScraper import *
 
 
-@bot.event
-async def on_message(message):
-      if message.channel.id == 1001772700294971392:
-  
-        
-      
+def main(url):
+    """
+    User Friendly Downloader for chegg homework help pages
 
-          
-            arg = message.content
-        
-        
-            path = Downloader.main(url=str(message.content))
-        #await asyncio.wait(3) 
-       # await message.channel.send(file=discord.File(path))
-            embed = discord.Embed(
-                  title="welcome",
-                  description="solution in DM",
-                  colour=discord.Colour(0xffff00))
-                     
-             
-            await message.channel.send(message.author.mention)
-            await message.channel.send(embed=embed)
-        
-        
-            await message.author.send(file=discord.File(path))
-        
-            await message.author.send(embed=embed)
-            await message.delete()
-        #await asyncio.wait(3)
-            os.remove(path=path)
-bot.run('OTg1MzA1NTc4MTI3NjUwODc3.GBVq8u.g2cuhPLpZjngwlLfT8BUtMr__-2qAA5lymQW3o')  
+    :return: Nothing
+    :rtype: None
+    """
+    conf = json.loads(read_text('cheggscraper', 'conf.json'))
 
+    default_save_file_format = conf.get('default_save_file_format')
+    default_cookie_file_path = conf.get('default_cookie_file_path')
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-c', '--cookie', default=default_cookie_file_path,
+                    help='path of cookie life', dest='cookie_file')
+    ap.add_argument('-u', '--url', help='url of chegg homework-help, put inside " "',
+                    type=str, dest='url')
+    # FIXME: DIFF TAGS FOR FILE FORMAT AND BASE PATH
+    ap.add_argument('-s', '--save',
+                    help='file path, where you want to save, put inside " " eg: test.html or'
+                         ' D:\\myFolder\\test.html or /home/test.html',
+                    type=str, default=default_save_file_format, dest='file_format')
+    args = vars(ap.parse_args())
+
+    if not os.path.exists(path=args['cookie_file']):
+        raise Exception(f'{args["cookie_file"]} does not exists')
+
+    if not args.get('url'):
+        args.update({'url': url})
+
+    Chegg = CheggScraper(cookie_path=args['cookie_file'])
+    return(Chegg.url_to_html(args['url'], file_name_format=args['file_format']))
